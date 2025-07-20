75e41f49ef99e97eb4383085dbc6a4a6fe476f96
Austin Melancon <armelan1@asu.edu>
2025-07-20 13:30:32 -0700

    Add .gitignore to ignore build artifacts

--- build/extern/CppUTest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/armel/OneDrive/github_projects/dsp-lib/extern/CppUTest
-# Build directory: C:/Users/armel/OneDrive/github_projects/dsp-lib/build/extern/CppUTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
